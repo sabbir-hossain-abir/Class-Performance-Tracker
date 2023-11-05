1e0c067c78ece8b17178788ecbd752aa75e8c56a
Sabbir Hossain Abir <2020-2-60-185@std.ewubd.edu>
2023-11-05 10:42:02 +0600

    Designed Signin, Signup and Class lecture XML

---
+++ app/src/main/java/com/example/cse489_2023_3_2020260185/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.cse489_2023_3_2020260185;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
